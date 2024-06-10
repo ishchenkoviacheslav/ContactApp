49f8d84490b813e29c38abce3a48bd93c7d07d4f
ishchenkoviacheslav <viacheslav.ishchenko@live.com>
2024-06-10 12:02:11 +0200

    Add project files.

---
+++ ContactApp/Models/Contact.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+
+namespace ContactApp.Models
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        private Guid id;
+        private string firstName;
+        private string lastName;
+        private DateOnly dateOfBirth;
+        private string company;
+
+        public Guid Id
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
+        }
+
+        public DateOnly DateOfBirth
+        {
+            get => dateOfBirth;
+            set
+            {
+                if (dateOfBirth != value)
+                {
+                    dateOfBirth = value;
+                    OnPropertyChanged(nameof(DateOfBirth));
+                }
+            }
+        }
+
+        public string Company
+        {
+            get => company;
+            set
+            {
+                if (company != value)
+                {
+                    company = value;
+                    OnPropertyChanged(nameof(Company));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ContactApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ContactApp.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ContactApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Contact> contacts;
+        private Contact selectedContact;
+
+        public ObservableCollection<Contact> Contacts
+        {
+            get => contacts;
+            set
+            {
+                if (contacts != value)
+                {
+                    contacts = value;
+                    OnPropertyChanged(nameof(Contacts));
+                }
+            }
+        }
+
+        public Contact SelectedContact
+        {
+            get => selectedContact;
+            set
+            {
+                if (selectedContact != value)
+                {
+                    selectedContact = value;
+                    OnPropertyChanged(nameof(SelectedContact));
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>
+        {
+            new Contact { Id = Guid.NewGuid(), FirstName = "John", LastName = "Doe", DateOfBirth = new DateOnly(1980, 1, 1), Company = "ABC Inc." },
+            new Contact { Id = Guid.NewGuid(), FirstName = "Jane", LastName = "Smith", DateOfBirth = new DateOnly(1975, 5, 15), Company = "XYZ Corp." }
+        };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
