332713fb2d464a90eb046dae1afe697260ad3d6e
ishchenkoviacheslav <viacheslav.ishchenko@live.com>
2024-06-12 15:54:36 +0200

    in progress

--- ContactApp/ViewModels/ContactsViewModel.cs
+++ ContactApp/ViewModels/ContactsViewModel.cs
@@ -5,7 +5,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace ContactApp.ViewModels
 {
